c777ee3c8e8412b9b6a75838f0d1fd4e2f8ca697
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-29 17:11:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 function Hidden_Number (num: number) {
-    if (num == Random_) {
+    if (Determiner == 1) {
+        Determiner += -1
+        return Tries
+    } else if (num == Random_) {
         return "NICE"
     } else if (num < Random_) {
-        Guesses += 1
+        Tries += 1
         return "HIGHER"
     } else if (num > Random_) {
-        Guesses += 1
+        Tries += 1
         return "LOWER"
     }
-    return Guesses
+    return Tries
 }
 input.onButtonPressed(Button.A, function () {
     if (User_Guess > 0) {
@@ -24,9 +27,15 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(User_Guess)
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("" + (Hidden_Number(Guesses)))
+    Determiner += 1
+    basic.showString("#Tries=")
+    basic.showString("" + (Hidden_Number(User_Guess)))
+    basic.pause(1000)
+    basic.clearScreen()
 })
 let User_Guess = 0
-let Guesses = ""
+let Tries = ""
+let Determiner = 0
 let Random_ = 0
 Random_ = randint(0, 20)
+Determiner = 0
