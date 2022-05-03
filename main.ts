0eed7ec853253f4a39f7ed21cb3ae48d8dff14a4
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-05-03 09:51:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonPressed(Button.A, function () {
         basic.showNumber(User_Guess)
     }
 })
+function Tries (num: number) {
+    return num
+}
 input.onButtonPressed(Button.AB, function () {
     basic.showString("" + (Hidden_Number(User_Guess)))
 })
@@ -12,31 +15,26 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(User_Guess)
 })
 input.onGesture(Gesture.Shake, function () {
-    Determiner += 1
     basic.showString("#Tries=")
-    basic.showString("" + (Hidden_Number(User_Guess)))
+    basic.showString("" + (Tries(Tries2)))
     basic.pause(1000)
     basic.clearScreen()
 })
 function Hidden_Number (num: number) {
-    if (Determiner == 1) {
-        Determiner += -1
-        return Tries
-    } else if (num == Random_) {
+    if (num == Random_) {
+        Tries2 = 0
         User_Guess = 0
         return "NICE"
     } else if (num < Random_) {
-        Tries += 1
+        Tries2 += 1
         return "HIGHER"
-    } else if (num > Random_) {
-        Tries += 1
+    } else {
+        Tries2 += 1
         return "LOWER"
     }
-    return Tries
 }
-let Tries = ""
+let Tries2 = 0
 let User_Guess = 0
-let Determiner = 0
 let Random_ = 0
 Random_ = randint(0, 20)
-Determiner = 0
+let Determiner = 0
